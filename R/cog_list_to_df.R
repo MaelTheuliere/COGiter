5f04c9b7330384bb8c4c5feff7728580a560d372
Maël Theulière <mael.theuliere@gmail.com>
2018-11-07 19:20:26 +0100

    - nouvelle version de cogifier : rajout d'un type liste de zone en sortie, possibilité de choisir les types de zonage en sortie - Nouvelle fonction cog_list_to_df

---
+++ R/cog_list_to_df.R
@@ -0,0 +1,81 @@
+#' Préparer les données pour passer d'un type liste à un type dataframe
+#'
+#' @param .data la table de données à convertire
+#' @param typezone le type de zonage
+#'
+#' @return la fonction renvoie une table de données renommée
+#' @export
+#' @import magrittr
+#' @importFrom dplyr mutate
+#' @importFrom dplyr select
+
+ConvertTypeZone<-function(.data,typezone) {
+  if (typezone=="communes") {
+    if (is.null(.data)){d<-NULL}
+    else {
+      d<-.data %>%
+        mutate(Zone=NOM_DEPCOM,CodeZone=DEPCOM,TypeZone="Communes") %>%
+        select(-NOM_DEPCOM,-DEPCOM) %>%
+        select(TypeZone,Zone,CodeZone,everything())
+    }
+  }
+  if (typezone=="epci") {
+    if (is.null(.data)){d<-NULL}
+    else {
+      d<-.data %>%
+        mutate(Zone=NOM_EPCI,CodeZone=EPCI,TypeZone="Epci") %>%
+        select(-NOM_EPCI,-EPCI) %>%
+        select(TypeZone,Zone,CodeZone,everything())
+    }
+  }
+  if (typezone=="departements") {
+    if (is.null(.data)){d<-NULL}
+    else {
+      d<-.data %>%
+        mutate(Zone=NOM_DEP,CodeZone=DEP,TypeZone="Départements") %>%
+        select(-NOM_DEP,-DEP) %>%
+        select(TypeZone,Zone,CodeZone,everything())
+    }
+  }
+  if (typezone=="regions") {
+    if (is.null(.data)){d<-NULL}
+    else {
+      d<-.data %>%
+        mutate(Zone=NOM_REG,CodeZone=REG,TypeZone="Régions") %>%
+        select(-NOM_REG,-REG) %>%
+        select(TypeZone,Zone,CodeZone,everything())
+    }
+  }
+  if (typezone=="metro") {
+    if (is.null(.data)){d<-NULL}
+    else {
+      d<-.data %>%
+        mutate(Zone="France métropolitaine",CodeZone="FRMETRO",TypeZone="France") %>%
+        select(TypeZone,Zone,CodeZone,everything())
+    }
+  }
+  if (typezone=="metrodrom") {
+    if (is.null(.data)){d<-NULL}
+    else {
+      d<-.data %>%
+        mutate(Zone="France métropolitaine et DROM",CodeZone="FRMETRODROM",TypeZone="France") %>%
+        select(TypeZone,Zone,CodeZone,everything())
+    }
+  }
+  return(d)
+}
+
+#' Convertir les données du COG d'un type liste à un type dataframe
+#'
+#' @param list la liste de données à convertir
+#'
+#' @return la fonction renvoie une table de données
+#' @export
+#' @import magrittr
+#' @importFrom purrr map2_df
+#' @importFrom dplyr mutate_at
+
+cog_list_to_df<-function(list) {
+  map2_df(list,names(list),~ ConvertTypeZone(.data = .x,typezone = .y)) %>%
+    mutate_at(vars(TypeZone,Zone,CodeZone),as.factor)
+}
--- R/cogifier.R
+++ R/cogifier.R
@@ -2,8 +2,12 @@
 #'
 #' @param .data la table de données à convertire
 #' @param code_commune le nom de la variable contenant le code commune sur 5 charactères
-#' @param metro booléen TRUE si on souhaite une aggrégation sur le contour de la France métropolitaine
-#' @param metrodrom booléen TRUE si on souhaite une aggrégation sur le contour de la France métropolitaine et des DROM
+#' @param communes booléen TRUE si on souhaite des données à la commune
+#' @param epci booléen TRUE si on souhaite des données à l'epci
+#' @param departements booléen TRUE si on souhaite des données au département
+#' @param regions booléen TRUE si on souhaite des données à la région
+#' @param metro booléen TRUE si on souhaite des données France métropolitaine
+#' @param metrodrom booléen TRUE si on souhaite des données France métropolitaine et des DROM
 #'
 #' @return la fonction renvoie une table de données
 #' @export
@@ -21,88 +25,73 @@
 #' @importFrom dplyr vars
 #' @importFrom rlang enquo
 #' @importFrom rlang !!
-#' @examples
-cogifier<-function(.data,code_commune=DEPCOM,metro=T,metrodrom=F){
-  quo_code_commune<-enquo(code_commune)
-  au_cog<-passer_au_cog_a_jour(.data=.data,code_commune=!!quo_code_commune)
-  if (metro==T & metrodrom==F) {
-    temp1<-au_cog %>%
-      dplyr::filter(!(REG %in% c("01","02","03","04","05","06"))) %>%
-      mutate(Zone="France métropolitaine" %>% as.factor,
-             CodeZone="FRMETRO" %>% as.factor,
-             TypeZone="France" %>% as.factor) %>%
-      select(-REG,-NOM_REG,-DEP,-NOM_DEP,-DEPCOM,-NOM_DEPCOM,-EPCI,-NOM_EPCI,-DEPARTEMENTS_DE_L_EPCI,-REGIONS_DE_L_EPCI) %>%
-      group_by_if(funs(!is.numeric(.))) %>%
-      summarise_if(is.numeric,funs(sum(.))) %>%
-      ungroup
-  }
-  if (metro==F & metrodrom==T) {
-    temp1<-au_cog %>%
-      mutate(Zone="France métropolitaine et DROM" %>% as.factor,
-             CodeZone="FRMETREDROM" %>% as.factor,
-             TypeZone="France" %>% as.factor) %>%
-      select(-REG,-NOM_REG,-DEP,-NOM_DEP,-DEPCOM,-NOM_DEPCOM,-EPCI,-NOM_EPCI,-DEPARTEMENTS_DE_L_EPCI,-REGIONS_DE_L_EPCI) %>%
-      group_by_if(funs(!is.numeric(.))) %>%
-      summarise_if(is.numeric,funs(sum(.))) %>%
-      ungroup
-  }
-  if (metro==T & metrodrom==T) {
-    temp1a<-au_cog %>%
-      dplyr::filter(!(REG %in% c("01","02","03","04","05","06"))) %>%
-      mutate(Zone="France métropolitaine" %>% as.factor,
-             CodeZone="FRMETRO" %>% as.factor,
-             TypeZone="France" %>% as.factor) %>%
-      select(-REG,-NOM_REG,-DEP,-NOM_DEP,-DEPCOM,-NOM_DEPCOM,-EPCI,-NOM_EPCI,-DEPARTEMENTS_DE_L_EPCI,-REGIONS_DE_L_EPCI) %>%
-      group_by_if(funs(!is.numeric(.))) %>%
-      summarise_if(is.numeric,funs(sum(.))) %>%
-      ungroup
-    temp1b<-au_cog %>%
-      mutate(Zone="France métropolitaine et DROM" %>% as.factor,
-             CodeZone="FRMETREDROM" %>% as.factor,
-             TypeZone="France" %>% as.factor) %>%
-      select(-REG,-NOM_REG,-DEP,-NOM_DEP,-DEPCOM,-NOM_DEPCOM,-EPCI,-NOM_EPCI,-DEPARTEMENTS_DE_L_EPCI,-REGIONS_DE_L_EPCI) %>%
-      group_by_if(funs(!is.numeric(.))) %>%
-      summarise_if(is.numeric,funs(sum(.))) %>%
-      ungroup
-    temp1<-bind_rows(temp1a,temp1b)
-  }
-  if (metro==F & metrodrom==F) {
-    temp1<-NULL
-  }
-  temp2<-au_cog %>%
-    mutate(Zone=NOM_REG,
-           CodeZone=REG,
-           TypeZone="Régions" %>% as.factor) %>%
-    select(-REG,-NOM_REG,-DEP,-NOM_DEP,-DEPCOM,-NOM_DEPCOM,-EPCI,-NOM_EPCI,-DEPARTEMENTS_DE_L_EPCI,-REGIONS_DE_L_EPCI) %>%
-    group_by_if(funs(!is.numeric(.))) %>%
-    summarise_if(is.numeric,funs(sum(.))) %>%
-    ungroup
-  temp3<-au_cog %>%
-    mutate(Zone=NOM_DEP,
-           CodeZone=DEP,
-           TypeZone="Départements" %>% as.factor) %>%
-    select(-REG,-NOM_REG,-DEP,-NOM_DEP,-DEPCOM,-NOM_DEPCOM,-EPCI,-NOM_EPCI,-DEPARTEMENTS_DE_L_EPCI,-REGIONS_DE_L_EPCI) %>%
-    group_by_if(funs(!is.numeric(.))) %>%
-    summarise_if(is.numeric,funs(sum(.))) %>%
-    ungroup
-  temp4<-au_cog %>%
-    mutate(Zone=NOM_EPCI,
-           CodeZone=EPCI,
-           TypeZone="Epci" %>% as.factor) %>%
-    select(-REG,-NOM_REG,-DEP,-NOM_DEP,-DEPCOM,-NOM_DEPCOM,-EPCI,-NOM_EPCI,-DEPARTEMENTS_DE_L_EPCI,-REGIONS_DE_L_EPCI) %>%
-    group_by_if(funs(!is.numeric(.))) %>%
-    summarise_if(is.numeric,funs(sum(.))) %>%
-    ungroup
-  temp5<-au_cog %>%
-    mutate(Zone=NOM_DEPCOM,
-           CodeZone=DEPCOM,
-           TypeZone="Communes" %>% as.factor) %>%
-    select(-REG,-NOM_REG,-DEP,-NOM_DEP,-DEPCOM,-NOM_DEPCOM,-EPCI,-NOM_EPCI,-DEPARTEMENTS_DE_L_EPCI,-REGIONS_DE_L_EPCI) %>%
-    group_by_if(funs(!is.numeric(.))) %>%
-    summarise_if(is.numeric,funs(sum(.))) %>%
-    ungroup
 
-  result<-bind_rows(temp1,temp2,temp3,temp4,temp5) %>%
-    mutate_at(vars(Zone,CodeZone,TypeZone),funs(as.factor))
-  result
+cogifier<-function(.data,code_commune=DEPCOM,
+                   communes=T,
+                   epci=T,
+                   departements=T,
+                   regions=T,
+                   metro=T,
+                   metrodrom=F,
+                   as_df=T){
+  quo_code_commune<-enquo(code_commune)
+  au_cog<-passer_au_cog_a_jour(.data=.data,code_commune=!!quo_code_commune,
+                               garder_info_supra = T)
+  c<-NULL
+  e<-NULL
+  d<-NULL
+  r<-NULL
+  m<-NULL
+  md<-NULL
+  if (communes==T) {
+    c<-au_cog %>%
+      select(-REG,-NOM_REG,-DEP,-NOM_DEP,-EPCI,-NOM_EPCI,-DEPARTEMENTS_DE_L_EPCI,-REGIONS_DE_L_EPCI) %>%
+      group_by_if(funs(!is.numeric(.))) %>%
+      summarise_if(is.numeric,funs(sum(.))) %>%
+      ungroup
+  }
+  if (epci==T) {
+    e<-au_cog %>%
+      select(-REG,-NOM_REG,-DEP,-NOM_DEP,-DEPCOM,-NOM_DEPCOM,-DEPARTEMENTS_DE_L_EPCI,-REGIONS_DE_L_EPCI) %>%
+      group_by_if(funs(!is.numeric(.))) %>%
+      summarise_if(is.numeric,funs(sum(.))) %>%
+      ungroup
+  }
+  if (departements==T) {
+    d<-au_cog %>%
+      select(-REG,-NOM_REG,-DEPCOM,-NOM_DEPCOM,-EPCI,-NOM_EPCI,-DEPARTEMENTS_DE_L_EPCI,-REGIONS_DE_L_EPCI) %>%
+      group_by_if(funs(!is.numeric(.))) %>%
+      summarise_if(is.numeric,funs(sum(.))) %>%
+      ungroup
+  }
+  if (regions==T) {
+    r<-au_cog %>%
+      select(-DEP,-NOM_DEP,-DEPCOM,-NOM_DEPCOM,-EPCI,-NOM_EPCI,-DEPARTEMENTS_DE_L_EPCI,-REGIONS_DE_L_EPCI) %>%
+      group_by_if(funs(!is.numeric(.))) %>%
+      summarise_if(is.numeric,funs(sum(.))) %>%
+      ungroup
+    if(metro==T) {
+      m<-au_cog %>%
+        dplyr::filter(!(REG %in% c("01","02","03","04","05","06"))) %>%
+        select(-REG,-NOM_REG,-DEP,-NOM_DEP,-DEPCOM,-NOM_DEPCOM,-EPCI,-NOM_EPCI,-DEPARTEMENTS_DE_L_EPCI,-REGIONS_DE_L_EPCI) %>%
+        group_by_if(funs(!is.numeric(.))) %>%
+        summarise_if(is.numeric,funs(sum(.))) %>%
+        ungroup
+    }
+    if(metrodrom==T) {
+      md<-au_cog %>%
+        select(-REG,-NOM_REG,-DEP,-NOM_DEP,-DEPCOM,-NOM_DEPCOM,-EPCI,-NOM_EPCI,-DEPARTEMENTS_DE_L_EPCI,-REGIONS_DE_L_EPCI) %>%
+        group_by_if(funs(!is.numeric(.))) %>%
+        summarise_if(is.numeric,funs(sum(.))) %>%
+        ungroup
+    }
+  }
+
+  result<-list("communes"=c,"epci"=e,"departements"=d,"regions"=r,"metro"=m,"metrodrom"=md)
+
+  if (as_df==T) {
+    result<-cog_list_to_df(result)
+  }
+
+  return(result)
 }
